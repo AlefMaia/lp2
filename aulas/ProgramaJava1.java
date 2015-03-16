2212c20d4ce848506f11b939d84595023552395a
AlefMaia <alefmaiamendes@hotmail.com>
2015-03-16 11:48:46 -0300

    Create ProgramaJava1.java
    Primeira aula de java

---
+++ aulas/ProgramaJava1.java
@@ -0,0 +1,6 @@
+public class ProgramaJava1{
+	public static void main(String [] args){
+		/* Imprime no console a frase: 'Meu primeiro programa' */
+		System.out.println("Meu primeiro programa java");
+	}
+}
