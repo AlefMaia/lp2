a23247870d00b70c315e39521bce5528f400d81d
AlefMaia <alefmaiamendes@hotmail.com>
2015-03-26 14:40:14 -0300

    Create Exponencial.java
    Exponencial.java

---
+++ exercicio2/Exponencial.java
@@ -0,0 +1,8 @@
+public class Exponencial{
+	public static void main(String [] args){
+		System.out.println("#  Q  C ");
+		for(int i=0;i<=10;i++){
+			System.out.println(i + "  " + (int)Math.pow(i,2) + "  " + (int)Math.pow(i,3));
+		}
+	}
+}
